9bbd92bd4d7f0059d6d903fad2351c2ef1d2601b
Adeel <arehman@acrobits.cz>
2021-06-18 09:06:08 +0500

    package updated

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "softphone-saas-objc-debug",
-            targets: ["Softphone", "Softphone_Swift", "ali"]),
+            targets: ["Softphone", "Softphone_Swift"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,15 +18,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/adeel/Softphone.xcframework-153555.zip",
-            checksum: "e509c3e42a4995248f2eaf1bd856092d29b5f616dacd2ce9f2b5199057213a93"),
+            url: "https://dist.acrobits.net/adeel/Softphone.xcframework.zip",
+            checksum: "3a3ad40f207a12620ab2e6c147832e6796b229c0efe1f52b69cfe7482782ff7a"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/adeel/Softphone_Swift.xcframework-153555.zip",
-            checksum: "eb355b7f7d7d0c6f0ac0bc2fbead7c38ae5694d7faee9c8dff6f85490bd77769"),
-        .binaryTarget(
-            name: "ali",
-            url: "https://dist.acrobits.net/adeel/ali.framework.zip",
-            checksum: "e3687d37ee3110ec5f575bbcb155854b67c62643cbdb577f90be7a5ce2940e68"),
+            url: "https://dist.acrobits.net/adeel/Softphone_Swift.xcframework.zip",
+            checksum: "258a5d3aa438e4ffd28be23bf6707b5d04df8d09d88208d7845634e999606db2"),
     ]
 )
